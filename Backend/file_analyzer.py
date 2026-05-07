70b524e9c7829108bc9038d6720fc8002b4dc8ed
Afek <afekaharoni@gmail.com>
2026-05-07 19:56:12 +0300

    Uploaded

--- Backend/file_analyzer.py
+++ Backend/file_analyzer.py
@@ -1,15 +1,24 @@
 import vt
 import hashlib
-from typing import Dict, Any, Optional
+from typing import Dict, Any
+from constants import SUSPICIOUS_STATUS, MALICIOUS_STATUS
+
 
 class FileAnalyzer:
-    def __init__(self, api_key):
+    def __init__(self, api_key: str):
         """
         Initializes the VirusTotal client
         Args:
             api_key (str): VirusTotal API key
         """
-        self.client = vt.Client(api_key)
+        self.api_key = api_key
+        self.client = None
+    
+    def set_client(self, client: vt.Client):
+        """
+        Sets the VirusTotal client as data member
+        """
+        self.client = client
     
     async def get_file_report_by_hash(self, file_content: bytes) -> Dict[str, Any]:
         """
@@ -19,14 +28,16 @@ class FileAnalyzer:
         Returns:
             Dict[str, Any]: A dictionary containing the analysis results
         """
+        if self.client is None:
+            return {"status": "error", "message": "Client not ready"}
         file_hash = hashlib.sha256(file_content).hexdigest()
         try:
             file_obj = await self.client.get_object_async(f"/files/{file_hash}")
             stats = file_obj.last_analysis_stats
             return {
                 "status": "found",
-                "Malicious": stats.get('malicious', 0),
-                "Suspicious": stats.get('suspicious', 0),
+                MALICIOUS_STATUS: stats.get('malicious', 0),
+                SUSPICIOUS_STATUS: stats.get('suspicious', 0),
                 "total_engines": sum(stats.values()),
                 "last_analysis_data": file_obj.last_analysis_date
                 }
--- Backend/main.py
+++ Backend/main.py
@@ -1,16 +1,26 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from pydantic import BaseModel
 from preprocessor import EmailPreprocessor
 from url_analyzer import URLAnalyzer
 from file_analyzer import FileAnalyzer
 from constants import (KEY_ACTUAL_URL, SAFE_STATUS, MALICIOUS_STATUS,
                         SUSPICIOUS_STATUS, VIRUS_TOTAL_API_KEY)
+from contextlib import asynccontextmanager
 import base64
 import uvicorn
+import vt
 
-app = FastAPI();
-file_analyzer = FileAnalyzer(VIRUS_TOTAL_API_KEY)
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """
+    Sets the client of the VirusTotal only when the server is up
+    """
+    async with vt.Client(VIRUS_TOTAL_API_KEY) as client:
+        analyzer = FileAnalyzer(VIRUS_TOTAL_API_KEY)
+        analyzer.set_client(client)
+        app.state.file_analyzer = analyzer
+        yield
 
 class Attachment(BaseModel):
     filename: str
@@ -23,13 +33,13 @@ class EmailData(BaseModel):
     sender: str 
     attachments: Optional[list[Attachment]] = []
 
+app = FastAPI(lifespan=lifespan);
 
 @app.post("/analyze")
-async def analyze_email(data: EmailData):
+async def analyze_email(data: EmailData, request: Request):
     reasons = []
     has_malicious = False
     could_not_check_all = False 
-
     # URL checker
     hyperlinks = EmailPreprocessor.extract_hyperlinks(data.body)
     raw_urls = EmailPreprocessor.extract_urls(data.body)
@@ -39,6 +49,7 @@ async def analyze_email(data: EmailData):
     for url in raw_urls:
         all_urls_to_check.add(url)
     for url in all_urls_to_check:
+        print(f"[LINK Check]:    Now checking link {url}")
         result = URLAnalyzer.is_malicious_url_by_google(url)
         if result is True:
             has_malicious = True
@@ -46,22 +57,27 @@ async def analyze_email(data: EmailData):
         elif result is None:
             could_not_check_all = True
             reasons.append(f"Could not verify the safety of: {url}")
-    
-    # Attachment checker
     if data.attachments:
         for att in data.attachments:
+            print(f"[FILE CHECK]:   Now checking file {att.filename}")
             try:
                 file_bytes = base64.b64decode(att.content)
-                report = await file_analyzer.get_file_report_by_hash(file_bytes)
+                report = await request.app.state.file_analyzer.get_file_report_by_hash(file_bytes)
                 if report.get("status") == "found":
-                    if report[MALICIOUS_STATUS] > 0:
+                    if report.get(MALICIOUS_STATUS, 0) > 0:
                         has_malicious = True
                         reasons.append(f"Malicious file detected: {att.filename}")
-                elif report[SUSPICIOUS_STATUS] > 0:
+                    elif report.get(SUSPICIOUS_STATUS, 0) > 0:
+                        could_not_check_all = True
+                        reasons.append(f"Suspicious file detected: {att.filename}")
+                    else:
+                        reasons.append(f'File "{att.filename}" was checked and is clean')
+                elif report.get("status") == "not_found":
                     could_not_check_all = True
-                    reasons.append(f"Suspicious file detected: {att.filename}")
-                else:
                     reasons.append(f"File {att.filename} is unknown to VirusTotal")
+                elif report.get("status") == "error":
+                    could_not_check_all = True
+                    reasons.append(f"System error: {report.get('message')}")
             except Exception as e:
                 could_not_check_all = True
                 reasons.append(f"Error analyzing file {att.filename}")
@@ -86,9 +102,6 @@ async def analyze_email(data: EmailData):
         "details": reasons
     }
 
-@app.on_event("shutdown")
-async def shutdown_event():
-    await file_analyzer.close()
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
